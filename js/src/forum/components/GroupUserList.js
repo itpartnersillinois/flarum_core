5400d7a992370b0a682061330f395b86d47d1256
Rohan Rodrigues <rohanrodrigues19@gmail.com>
2020-10-21 17:51:52 -0500

    Fix print statement

--- js/src/forum/components/GroupUserList.js
+++ js/src/forum/components/GroupUserList.js
@@ -151,7 +151,9 @@ export default class GroupUserList extends Component {
   }
 
   loadUsers() {
-    console.log("TEST: " + app.store.pushPayload('users'));
+    console.log("TEST: " + app.
+    store.
+    pushPayload('users'));
     
    // app.store.find('users');
   //  console.log("Total Length of users " + app.store.all('users').length);
